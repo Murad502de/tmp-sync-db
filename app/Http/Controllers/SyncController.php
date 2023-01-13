b43d6d00c50f9677ec6693d0d55eecc87016fd67
itWelt <rassulovmurad@gmail.com>
2023-01-14 02:44:23 +0300

    deploy

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -10,8 +10,8 @@ use Illuminate\Http\Response;
 class SyncController extends Controller
 {
     private static $AMO_API = null;
-    private static $FROM    = 750;
-    private static $TO      = 800;
+    private static $FROM    = 800;
+    private static $TO      = 816;
 
     // tottal 816
 
